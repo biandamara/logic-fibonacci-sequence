8032208b50fc6cf2e7b1c61f26062e4a7736d1b9
Bian Damara <99477664+biandamara@users.noreply.github.com>
2023-01-01 21:34:01 +0700

    Update

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const number = parseInt(prompt('Enter the number of terms: '));
-let n1 = 0, n2 = 1, nextTerm;
+function fib(n) {
+  // jika n kurang dari 3, maka hasilnya adalah 1
+	if (n < 3) return 1;
 
-console.log('Fibonacci Series:');
+	// tetapkan nilai sebelumnya dan saat ini ke 1
+	let prev = 1;
+	let curr = 1;
 
-for (let i = 1; i <= number; i++) {
-    console.log(n1);
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
+	// mulai loop dari 2 hingga n-1
+	for (let i = 2; i < n; i++) {
+		// hitung angka selanjutnya dengan menjumlahkan nilai sebelumnya dan saat ini
+		const next = prev + curr;
+		// tetapkan nilai sebelumnya menjadi saat ini
+		prev = curr;
+		// tetapkan nilai saat ini menjadi angka selanjutnya
+		curr = next;
+	}
+
+	// kembalikan nilai saat ini sebagai hasil
+	return curr;
 }
+
+console.log(fib(1)); // 1
+console.log(fib(2)); // 1
+console.log(fib(3)); // 2
+console.log(fib(4)); // 3
+console.log(fib(5)); // 5
